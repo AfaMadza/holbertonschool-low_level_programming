44466d6c7d67156dac0746a18173b64744196a29
AfaMadza <305@holbertonschool.com>
2018-01-17 23:47:07 +0000

    Fixed typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
  * main - Returns the size of types
  *
  * Description: Returns size of all types on a computer
- *section header: Prints out sizes next to types
+ * Section header: Prints out sizes next to types
  * Return: Returns a value of 0
 */
+
 int main(void)
 {
 	char c;
@@ -14,7 +15,7 @@ int main(void)
 	long long int M;
 	float f;
 
-	printf("Size of char: %lu byte(s)\n", (long int) sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (long int) sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (long int) sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (long int) sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", (long int) sizeof(M));
