66b475b45115b897d739f69328363270f14878cf
AfaMadza <305@holbertonschool.com>
2018-01-17 23:00:35 +0000

    Changed unsigned long to long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long int M;
 	float f;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(M));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of char: %lu byte(s)\n", (long int)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (long int)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (long int)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (long int)sizeof(M));
+	printf("Size of a float: %lu byte(s)\n", (long int)sizeof(f));
 	return (0);
 }
