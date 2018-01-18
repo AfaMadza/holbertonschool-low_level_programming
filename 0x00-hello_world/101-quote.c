1a642ba369f0204dbcf88492c8110ff9b13d709c
AfaMadza <305@holbertonschool.com>
2018-01-18 00:53:15 +0000

    Write to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Returns a string
+ *
+ * Description: Returns a string to stderr
+ * Section header: Prints out a string
+ * Return: Returns a value of 0
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
