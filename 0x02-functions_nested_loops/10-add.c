e7a7262164dd0f11a42d6598e17a169f9f326a44
AfaMadza <305@holbertonschool.com>
2018-01-23 18:32:15 +0000

    Sum two ints. Parameters in description.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* add - adds two ints
+* @x: first int
+* @y: second int
+* Return: Two values added
+*/
+int add(int x, int y)
+{
+	int z;
+
+	z = (x + y);
+
+	return (z);
+}
