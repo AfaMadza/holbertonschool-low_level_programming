8feef6124200dd48d2f9f5f47923084b4f1fe4b1
AfaMadza <305@holbertonschool.com>
2018-01-24 05:57:38 +0000

    First 50 of Fibonnaci sequence excluding 0 and 1 with for loop.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print first 50 fibonnaci numbers excluding 0 and 1
+* Return: 0
+*/
+int main(void)
+{
+	long long int x = 0, y = 1, z, i;
+
+	for (i = 2; i <= 50; i++)
+	{
+		z = x + y;
+		printf("%llu", z);
+		if (i != 50)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x = y;
+		y = z;
+	}
+	putchar('\n');
+	return (0);
+}
