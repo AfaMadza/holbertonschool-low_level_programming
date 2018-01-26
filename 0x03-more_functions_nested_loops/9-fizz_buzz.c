9116de7ea5bf441558c034b70e28adbf6facd1fb
AfaMadza <305@holbertonschool.com>
2018-01-26 04:53:12 +0000

    Switched else to else if and added conditions

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((x % 3 == 0) && (x % 5 != 0))
+		if ((x % 3 == 0) && (x % 5 != 0))
 		{
 			printf("Fizz ");
 		}
@@ -21,7 +17,11 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else
+		else if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((x % 3 != 0) && (x % 5 != 0))
 		{
 			printf("%i ", x);
 		}
