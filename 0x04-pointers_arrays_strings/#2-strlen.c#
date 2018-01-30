63b615287ec21010bcf8a11d757ab28a0831b486
AfaMadza <305@holbertonschool.com>
2018-01-30 23:31:53 +0000

    Added _strlen to file

---
+++ 0x04-pointers_arrays_strings/#2-strlen.c#
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: pointer to string whose length will be returned
+ * Return: None
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -26,3 +26,20 @@ void puts_half(char *str)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: pointer to string whose length will be returned
+ * Return: None
+ */
+int _strlen(char *s)
+{
+        int length = 0;
+
+        for (; *s != '\0'; s++)
+        {
+                length++;
+        }
+        return (length);
+}
