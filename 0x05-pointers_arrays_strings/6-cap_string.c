647f69e97e9d9001390ee50a952efbf8ca9d4ccc
AfaMadza <305@holbertonschool.com>
2018-02-02 07:48:13 +0000

    Added prototype to header

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,32 @@
 #include "holberton.h"
 /**
+* cap_string - capitalize all words of string.
+*
+* @s: pointer to string
+* Return: string.
+*/
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		if (isspecial(s[i]) == 1)
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = (s[i + 1]) - 32;
+			}
+		}
+
+	}
+	return (s);
+}
+/**
 * isspecial - checks to see if a character is special.
 *
 * @x: character to be evaluated
@@ -27,31 +54,4 @@ int isspecial(char x)
 	default:
 		return (0);
 	}
-}
-/**
-* cap_string - capitalize all words of string.
-*
-* @s: pointer to string
-* Return: string.
-*/
-char *cap_string(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ( s[0] >= 'a' && s[0] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
-		if (isspecial(s[i]) == 1)
-		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				s[i + 1] = (s[i + 1]) - 32;
-			}
-		}
-
-	}
-	return (s);
 }
--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *s)
 	int i = 0;
 	char *p = s;
 	int j;
-       	char text[] = {'A', 'a', 'E', 'e', 'o', 'O', 't', 'T', 'l', 'L'};
+	char text[] = {'A', 'a', 'E', 'e', 'o', 'O', 't', 'T', 'l', 'L'};
 	char code[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (j = 0; s[j] != '\0'; j++)
