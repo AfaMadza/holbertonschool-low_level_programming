db52b859b950a05fbc8d49a1f68d58098488fc06
AfaMadza <305@holbertonschool.com>
2018-02-02 05:05:45 +0000

    Revised logical operators and increments

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -40,12 +40,11 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (isspecial(s[i]) == 1 && isspecial(s[i + 1]) != 1)
+		if (isspecial(s[i]) == 1 /*&& isspecial(s[i + 1]) != 1*/)
 		{
-			i++;
-			if (s[i] >= 97 && s[i] <= 122)
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
-				s[i] = s[i] - 32;
+				s[i + 1] = s[i + 1] - 32;
 			}
 		}
 
