044787b6fe72bfca3c67350d30852b488dadbafc
AfaMadza <305@holbertonschool.com>
2018-02-05 21:40:36 +0000

    Removed unnecessary blocks

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -10,27 +10,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int length = _strlen(src);
 
 	for (i = 0 ; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	dest[length + i] = '\0';
-
 	return (dest);
-}
-/**
- * _strlen - returns length of a string
- *
- * @s: pointer to string whose length will be returned
- * Return: None
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	for (; *s != '\0'; s++)
-	{
-		length++;
-	}
-	return (length);
 }
