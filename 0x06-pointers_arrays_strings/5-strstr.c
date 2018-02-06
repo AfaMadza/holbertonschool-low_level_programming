09a5706696125363e93df8e24a3edde5001d05e8
AfaMadza <305@holbertonschool.com>
2018-02-06 05:29:33 +0000

    Fixed betty format issues

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -11,21 +11,21 @@ char *_strstr(char *haystack, char *needle)
 	char *a, *b;
 
 	b = needle;
-	if (*b == 0) {
+	if (*b == 0)
 		return (haystack);
-	}
-	for (; *haystack != 0; haystack++) {
-		if (*haystack != *b) {
+	for (; *haystack != 0; haystack++)
+	{
+		if (*haystack != *b)
 			continue;
-		}
 		a = haystack;
-		while (1) {
-			if (*b == 0) {
+		while (1)
+		{
+			if (*b == 0)
+			{
 				return (haystack);
 			}
-			if (*a++ != *b++) {
+			if (*a++ != *b++)
 				break;
-			}
 		}
 		b = needle;
 	}
