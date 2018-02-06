2e2c5103d950220c8abf20785960fe3316fe5079
AfaMadza <305@holbertonschool.com>
2018-02-06 05:04:17 +0000

    Pointer, array and strings ops

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -4,17 +4,23 @@
  * print_diagsums - prints sums of the two diagonals of a square matrix.
  *
  * @a: pointer to a multi-dimensional array.
+ * @size: size of array.
  * Return: None
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum, sum2;
+	int sum = 0;
+	int sum2 = 0;
 
-	for (i = 0; a[i] < size; i++)
+	for (i = 0; i < (size * size); i = i + size + 1)
 	{
-		sum = sum + x[i][i];
-		sum2 = sum2 + x[i][size - i -1];
+		sum = sum + a[i];
+	}
+	for (i = size - 1; i < (size * size) && i < size * (size - 1)
+		     ; i = i + size - 1)
+	{
+		sum2 = sum + a[i];
 	}
 	printf("%d, ", sum);
 	printf("%d\n", sum2);
