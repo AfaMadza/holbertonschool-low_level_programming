47d263888530ce863f5aec5718b18887186b79e4
AfaMadza <305@holbertonschool.com>
2018-02-13 23:47:28 +0000

    Added malloc check within for loop

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -15,13 +15,15 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	s = malloc(sizeof(int *) * width);
-	if (s == NULL);
-	return (NULL);
+
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		s[i] = malloc(sizeof(int) * height);
-		if (s[i] == 0)
+		if (s[i] == NULL)
 			return (NULL);
 		for (j = 0; j < height; j++)
 		{
