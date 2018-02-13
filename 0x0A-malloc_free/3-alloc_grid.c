c033fc5827e479cd1dd1797d7de0efb32e1748d3
AfaMadza <305@holbertonschool.com>
2018-02-13 23:06:58 +0000

    Removed extra space after '('

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	s = malloc(sizeof(int *) * width);
-	if ( s == NULL)
+	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
