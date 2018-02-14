045ef30551e6891d6d2786e44756fadd3b8f657a
AfaMadza <305@holbertonschool.com>
2018-02-14 03:15:57 +0000

    Switched width and height in every instance

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	s = malloc(width * sizeof(int *));
+	s = malloc(height * sizeof(int *));
 
 	if (s == NULL)
 	{
