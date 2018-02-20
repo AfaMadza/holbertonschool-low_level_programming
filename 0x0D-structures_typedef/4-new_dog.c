1ccd75932f9339965c977a500d5bad8cde19b491
AfaMadza <305@holbertonschool.com>
2018-02-20 00:50:48 +0000

    Added NULL checks for name and owner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -33,19 +33,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(n);
 		return (NULL);
 	}
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+        {
+                free(n);
+                free(o);
+                return (NULL);
+        }
 	for (i = 0; name[i] != '\0'; i++)
 		n[i] = name[i];
 	n[i] = '\0';
 	for (i = 0; owner[i] != '\0'; i++)
 		o[i] = owner[i];
 	o[i] = '\0';
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
-		free(n);
-		free(o);
-		return (NULL);
-	}
+
 	new_dog->name = n;
 	new_dog->age = age;
 	new_dog->owner = o;
