a7ffd9c47b87a5ef2b1332831aa1360e58d43f5e
AfaMadza <305@holbertonschool.com>
2018-02-20 00:21:32 +0000

    Added free for each instance of malloc

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -4,20 +4,45 @@
 /**
 * new_dog - creates a struct called new_dog
 * @name: name of new_dog.
-* @age: age od new_dog;
+* @age: age of new_dog;
 * @owner: owner of new dog;
 * Return: None.
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL || d->owner == NULL)
+	unsigned int i;
+	char *n;
+	char *o;
+	dog_t *new_dog;
+
+	n = malloc(sizeof(name));
+	if (n == NULL)
 	{
-		printf("(nil)\n");
+		return (NULL);
 	}
-	printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+	o = malloc(sizeof(owner));
+	if (o == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		n[i] = name[i];
+	n[i] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
+		o[i] = owner[i];
+	o[i] = '\0';
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		free(n);
+		free(o);
+		return (NULL);
+	}
+	new_dog->name = n;
+	new_dog->age = age;
+	new_dog->owner = o;
+	return (new_dog);
+
 }
