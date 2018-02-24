b180005133bea1fdf8aaa576a28768c1fcdffa9a
AfaMadza <305@holbertonschool.com>
2018-02-24 20:45:51 +0000

    Removed separator NULL check from first if statement

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arguments;
 
-	if (n == 0 || separator == NULL)
+	if (n == 0)
 		return;
 
 	va_start(arguments, n);
