8eb821d8245f2d456c4ce93970b5c93599d10da7
Afa Madza <305@holbertonschool.com>
2018-03-06 19:08:43 +0000

    Changed return values to 0 and added NULL test

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp;
 	listint_t *next;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	temp = *head;
 
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (; temp != NULL && i < index - 1; i++)/*iterate to node before index*/
 		temp = temp->next;
 	if (temp == NULL || temp->next == NULL)/*for invalid index*/
-		return (-1);
+		return (0);
 	next = temp->next->next;
 	/*temp->next is the node we want to delete, so we need to store the*/
 	  /*  pointer for the node after temp->next*/
