2554ad462fde572436c77c1f28c67fb50d8ce65d
Afa Madza <305@holbertonschool.com>
2018-03-09 01:05:44 +0000

    Used & to find bit value

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *get_bit - returns the value of a bit at a given index.
+ *@n: pointer to binary number.
+ *@index: index whose bit value should be returned.
+ * Return: value of bit at index or -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (((n >> index) & 1) == 0 || ((n >> index) & 1) == 1)
+	{
+		bit = (n >> index) & 1;
+		return (bit);
+	}
+	return (-1);
+}
