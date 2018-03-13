eaaf842adbfb80040e3193b8badfdef2b2c2d72e
Afa Madza <305@holbertonschool.com>
2018-03-13 23:00:40 +0000

    Reworked logic to optimize code

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,20 +1,5 @@
 #include "holberton.h"
 /**
- * exists - checks to see if a file exists.
- *@filename: pointer to file.
- *Return: 0 or -1 for success or failure.
- */
-int exists(const char *filename)
-{
-	int fd = -1;
-
-	fd = open(filename, O_RDWR);
-	if (fd < 0)
-		return (-1);
-	close(fd);
-	return (0);
-}
-/**
  * string_len - finds length of string.
  *@s: pointer to string.
  *Return: string length.
@@ -38,32 +23,17 @@ int string_len(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len;
-	char *empty = "";
 
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 	len = string_len(text_content);
 
-	if ((exists(filename)) == -1)
-	{
-		fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-		if (text_content == NULL)
-		{
-			write(fd, empty, 0);
-			close(fd);
-			return (1);
-		}
-	}
-	else if ((exists(filename)) == 0)
-	{
-		fd = open(filename, O_RDWR | O_TRUNC);
-		if (text_content == NULL)
-		{
-			write(fd, empty, 0);
-			close(fd);
-			return (1);
-		}
-	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 	write(fd, text_content, len);
 	close(fd);
 	return (1);
