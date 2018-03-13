81b617d0d9309acbb442199582ceaa4d09791094
Afa Madza <305@holbertonschool.com>
2018-03-13 23:50:29 +0000

    Added returns for text_content NULL

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -26,23 +26,16 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	len = string_len(text_content) + 1;
+	if (text_content == NULL)
+		text_content = "";
+
+	len = string_len(text_content);
 
 	fd = open(filename, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
-		if (text_content == NULL)
-		{
-			close(fd);
-			return (-1);
-		}
 		close(fd);
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
 	}
 	write(fd, text_content, len);
 	close(fd);
