3f24b0f40936d7c578676ecdbdba3c4985eb474e
Afa Madza <305@holbertonschool.com>
2018-03-14 02:12:15 +0000

    Added check for write failure

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,21 +15,28 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(argv[1], O_RDWR, S_IRUSR | S_IWUSR);
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
-		   S_IRGRP | S_IWGRP);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+		   S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((char_count = read(fd1, buf, sizeof(buf))) != 0)
-		write(fd2, buf, char_count);
+	while ((char_count = read(fd1, buf, sizeof(buf))) > 0)
+	{
+		char_count = write(fd2, buf, char_count);
+		if (char_count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 	if ((close(fd1)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
