7faf728ceea955908cd480a6df1758be048e6cc3
Afa Madza <305@holbertonschool.com>
2018-03-14 01:49:11 +0000

    Added checks for failed close

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * main - copies the contents of a file from source to dest.
+ *@argv: arguments passed.
+ *@argc: number of arguments.
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, char_count;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDWR, S_IRUSR | S_IWUSR);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+		   S_IRGRP | S_IWGRP);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((char_count = read(fd1, buf, sizeof(buf))) != 0)
+		write(fd2, buf, char_count);
+	if ((close(fd1)) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if ((close(fd2)) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	return (0);
+}
