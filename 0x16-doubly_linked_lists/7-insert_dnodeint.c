00994c23ce02213ffdd67022c7924bdfebce50ff
Afa Madza <305@holbertonschool.com>
2018-04-15 19:13:29 +0000

    Added edge case tests

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,11 +10,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *idx_node, *new_node;
 
+	if (h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
 	node = *h;
 	idx_node = get_dnodeint_at_index(node, idx);
 	idx_node = idx_node->prev;
