60a47a5ed2298845944ce4b08f165f52a2104881
Afa Madza <305@holbertonschool.com>
2018-04-15 18:53:59 +0000

    Removed list length function

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next->prev = new_node;
 	return (new_node);
 }
+#include <stdio.h>
 /**
  * get_dnodeint_at_index - returns the node at a given index
  * @head: pointer to head of linked list.
@@ -33,46 +34,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *node = head;
-	unsigned int counter = 0;
+	unsigned int counter = 1;
 
-	if (index > dlistint_len(node))
+	if (head == NULL)
 		return (NULL);
-	while (node->next != NULL && counter != index)
+	while (counter < index)
 	{
-		node = node->next;
 		counter++;
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
-	return (node);
-}
-/**
- * dlistint_len - returns the number of elements in a doubly  linke list
- * @h: pointer to head of linked list.
- * Return: number of nodes in list.
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t node_num = 0;
-	const dlistint_t *node = NULL;
-
-	if (h == NULL)
-		return (0);
-	node = h;
-	if (node->prev == NULL)
-	{
-		while (node != NULL)
-		{
-			node = node->next;
-			node_num++;
-		}
-	}
-	else
-	{
-		while (node != NULL)
-		{
-			node = node->prev;
-			node_num++;
-		}
-	}
-	return (node_num);
+	return (head);
 }
