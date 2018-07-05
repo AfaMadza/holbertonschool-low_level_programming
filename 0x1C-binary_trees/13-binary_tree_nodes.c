040fe07b3e9f666fffae1b1e75ac1168889eb9e9
Afa Madza <305@holbertonschool.com>
2018-07-05 21:21:31 +0000

    Removed extra condition checking for parents

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -11,9 +11,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left || tree->right) && tree->parent)
-		return (1);
-
 	if (!tree->left && !tree->right)
 		return (0);
 	return (binary_tree_nodes(tree->left) +
